0f9c572d28589decf591cd2c4cbe8ec3104a16e6
MarceloVichar <marcos.marcelo43@gmail.com>
2022-05-15 17:18:55 -0300

    component stocks finished

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,10 +1,39 @@
 <template>
-    <div>
-        <h1>stock</h1>
-    </div>
+    <v-flex class="pb-3 pr-3" xs12 md6 lg4>
+        <v-card class="green darken-3 white--text">
+            <v-card-title class="headline">
+                <strong>{{stock.name}} <small>(Preço: {{stock.price}})</small></strong>
+            </v-card-title>
+        </v-card>
+        <v-card>
+            <v-container fill-height>
+                <v-text-field label="quantidade" type="number" v-model.number="quantity" />
+                <v-btn class="green darken-3 white--text" @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)">Comprar</v-btn>
+            </v-container>
+        </v-card>
+    </v-flex>
 </template>
 <script>
-export default {};
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0,
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      };
+      // eslint-disable-next-line no-console
+      console.log(order);
+      this.quantity = 0;
+    },
+  },
+};
 </script>
 <style>
 </style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,24 @@
 <template>
-    <div>
-       <h1>stocks</h1> 
-    </div>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+    </v-layout>
 </template>
 <script>
-export default {};
+import Stock from "./Stock.vue";
+
+export default {
+  components: { Stock },
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: "110" },
+        { id: 2, name: "WM", price: "120" },
+        { id: 3, name: "APPLE", price: "200" },
+        { id: 4, name: "OUTRA", price: "50" },
+      ],
+    };
+  },
+};
 </script>
 <style>
 </style>
